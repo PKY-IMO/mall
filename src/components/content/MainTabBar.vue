e264b3ccd388b051c8c2a48896541c4b1b562468
pankeyu <1029641620@qq.com>
2021-05-11 17:02:53 +0800

    导航栏+首页

---
+++ src/components/content/MainTabBar.vue
@@ -0,0 +1,43 @@
+<template>
+     <div class="main-tab-bar"> 
+     <tab-bar>
+      <tab-bar-item path='/home'>
+          <img slot="item-icon" src="~assets/img/tabbar/cc-home.png" alt="">
+          <img slot="item-icon-active" src="~assets/img/tabbar/cc-home1.png" alt="">
+          <div slot="item-text">首页</div>
+      </tab-bar-item>
+      <tab-bar-item path='/category'>
+          <img slot="item-icon" src="~assets/img/tabbar/category.png" alt="">
+          <img slot="item-icon-active" src="~assets/img/tabbar/category1.png" alt="">
+          <div slot="item-text">分类</div>
+      </tab-bar-item>
+      <tab-bar-item path='/cart'>
+          <img slot="item-icon" src="~assets/img/tabbar/shop-cart-1.png" alt="">
+          <img slot="item-icon-active" src="~assets/img/tabbar/shop-cart-11.png" alt="">
+          <div slot="item-text">购物车</div>
+      </tab-bar-item>
+      <tab-bar-item path='/profile'>
+          <img slot="item-icon" src="~assets/img/tabbar/profile.png" alt="">
+          <img slot="item-icon-active" src="~assets/img/tabbar/profile1.png" alt="">
+          <div slot="item-text">我的</div>
+      </tab-bar-item>
+    </tab-bar>
+    </div>
+    
+</template>
+
+<script>
+import TabBar from 'components/common/tabbar/TabBar'
+import TabBarItem from 'components/common/tabbar/TabBarItem'
+export default {
+  name: "MainTabBar",
+  components: {
+    TabBar,
+    TabBarItem
+  }
+}
+</script>
+
+<style>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,11 @@
-import { createApp } from 'vue'
+import Vue from 'vue'
+import VueRouter from 'vue-router'
 import App from './App.vue'
+import router from './router'
 
-createApp(App).mount('#app')
+Vue.config.productionTip = false
+
+new Vue({
+  render: h => h(App),
+  router
+}).$mount('#app')
